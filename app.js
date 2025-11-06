1d2a1c38dea92d304e177a95df834b019befa97d
sai kiran reddy <152466057+SaiKiranReddy72@users.noreply.github.com>
2025-11-05 23:28:38 -0700

    Update app.js

--- app.js
+++ app.js
@@ -50,9 +50,9 @@ app.engine('ejs', ejsMate);
 app.use(express.static(path.join(__dirname, "/public")));
 
 
-app.get("/", (req, res) => {
-  res.send("Hi, I am root");
-});
+// app.get("/", (req, res) => {
+//   res.send("Hi, I am root");
+// });
 
 const store =MongoStore.create({
   mongoUrl:dbUrl,
